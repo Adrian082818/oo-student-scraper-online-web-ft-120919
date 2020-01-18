778709d888c9721a3981f6bf0439b3066af00ce8
Adrian Mohabir <mohabiradrian@gmail.com>
2020-01-18 15:58:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,9 +6,9 @@ class Student
 
   def initialize(student_hash)
     student_hash.each do |key, value|
-      self.send (("#{key}="), value)
-      @@all << self 
+      self.send ("#{key}=", value)
   end
+      @@all << self 
 end 
 
   def self.create_from_collection(students_array)
