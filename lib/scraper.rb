c5d650f0b8249ba9cb85cf9d71f5fbb7bda45f93
Adrian Mohabir <mohabiradrian@gmail.com>
2020-01-18 15:54:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -44,10 +44,10 @@ class Scraper
           return_hash[:blog] = element.attr('href')
         end
       end
-      return_hash[:profile_quote] = doc.css(".vitals-container .vitals-text-container .profile-quote").text
-      return_hash[:bio] = doc.css(".bio-block.details-block .bio-content.content-holder .description-holder p").text
+  #     return_hash[:profile_quote] = doc.css(".vitals-container .vitals-text-container .profile-quote").text
+  #     return_hash[:bio] = doc.css(".bio-block.details-block .bio-content.content-holder .description-holder p").text
 
-  return_hash
+  # return_hash
   end
 end 
   #   social_url = doc.css(".vitals-container .social-icon-container a")
