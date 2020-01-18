26fefb9ee3b9f91097dc386edf466205f8c98202
Adrian Mohabir <mohabiradrian@gmail.com>
2020-01-18 15:41:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,39 +29,39 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    students_hash = {}
-      html = open(profile_url)
-      doc = Nokogiri::HTML(html)
-       origin = doc.css("div.social-icon-controler a")
-       origin.each do |student|
-        url = student.attribute("href")
-        students_hash[:twitter_url] = url if url.include?("twitter")
-        students_hash[:linkedin_url] = url if url.include?("linkedin")
-        students_hash[:github_url] = url if url.include?("github")
-        students_hash[:blog_url] = url if student.css("img").attribute("src").text.include?("rss")
-    end
-        students_hash[:profile_quote] = html.css("div.profile-quote").text
-        students_hash[:bio] = html.css("div.bio-content p").text
-    students_hash
-  end
-  #   student = {}
-  # profile_page = Nokogiri::HTML(open(profile_url))
-  # links = profile_page.css(".social-icon-container").children.css("a").map {|el| el.attribute("href").value}
-  # links.each do |link|
-  #   if link.include?("linkedin")
-  #     student[:linkedin] = link
-  #     elsif link.include?("github")
-  #     student[:github] = link 
-  #     elsif link.include?("twitter")
-  #     student[:twitter] = link 
-  #   else 
-  #     student[:blog] = link 
-  #     student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
-  #     student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
-  #   student 
-  #   end 
-  # end 
+    # students_hash = {}
+    #   html = open(profile_url)
+    #   doc = Nokogiri::HTML(html)
+    #   origin = doc.css("div.social-icon-controler a")
+    #   origin.each do |student|
+    #     url = student.attribute("href")
+    #     students_hash[:twitter_url] = url if url.include?("twitter")
+    #     students_hash[:linkedin_url] = url if url.include?("linkedin")
+    #     students_hash[:github_url] = url if url.include?("github")
+    #     students_hash[:blog_url] = url if student.css("img").attribute("src").text.include?("rss")
+    # end
+    #     students_hash[:profile_quote] = html.css("div.profile-quote").text
+    #     students_hash[:bio] = html.css("div.bio-content p").text
+    # students_hash
   # end
+    student = {}
+  profile_page = Nokogiri::HTML(open(profile_url))
+  links = profile_page.css(".social-icon-container").children.css("a").map {|el| el.attribute("href").value}
+  links.each do |link|
+    if link.include?("linkedin")
+      student[:linkedin] = link
+      elsif link.include?("github")
+      student[:github] = link 
+      elsif link.include?("twitter")
+      student[:twitter] = link 
+    else 
+      student[:blog] = link 
+      student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
+      student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
+    student 
+    end 
+  end 
+  end
 
 end
 
